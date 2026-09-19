53358fe42165821358262fb645d769363a6dad30
David Lima <davidlimma05@gmail.com>
2026-09-19 19:18:34 -0300

    chore: commit inicial

---
+++ src/main/java/com/registraai/registro_coristas_api/RegistroCoristasApiApplication.java
@@ -0,0 +1,13 @@
+package com.registraai.registro_coristas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroCoristasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroCoristasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/registraai/registro_coristas_api/RegistroCoristasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registraai.registro_coristas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroCoristasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
